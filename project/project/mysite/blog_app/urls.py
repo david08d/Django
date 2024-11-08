8e25cb7198abecc46377805be5ff1cf0d28e203d
David <diwtech08@gmail.com>
2024-11-08 10:52:49 +0200

    blogg_app_updated

--- project/project/mysite/blog_app/urls.py
+++ project/project/mysite/blog_app/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 app_name = "blog_app"
 urlpatterns = [
     path('posts/', BlogListView.as_view(), name='posts'),
-    path('post/<int:pk>',BlogDetailView.as_view(),name='post'),
-    path('post_delete/<int:pk>',BlogDeleteView.as_view(),name='delete_post'),
-    path('update_post/<int:pk>',BlogUpdateView.as_view(),name='update_post'),
+    path('post/<int:pk>', BlogDetailView.as_view(), name='post'),
+    path('post_delete/<int:pk>', BlogDeleteView.as_view(), name='delete_post'),
+    path('update_post/<int:pk>', BlogUpdateView.as_view(), name='update_post'),
+    path('create_post/', BlogPostCreateView.as_view(), name='create_post'),
 ]
--- project/project/mysite/blog_app/views.py
+++ project/project/mysite/blog_app/views.py
@@ -2,42 +2,53 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from .models import BlogPost
-from django.views.generic import DetailView,UpdateView,DeleteView
+from django.views.generic import DetailView, UpdateView, DeleteView, CreateView
+
+
 class BlogListView(ListView):
+    paginate_by = 2
     model = BlogPost
+
+
 
 class BlogDetailView(DetailView):
     model = BlogPost
 
+
 class BlogUpdateView(UpdateView):
     model = BlogPost
-    fields = ["title","text"]
+    fields = ["title", "text"]
 
     def get_queryset(self, **kwargs):
         qset = super().get_queryset(**kwargs)
-        qset = qset.filter(owner = self.request.user)
+        qset = qset.filter(owner=self.request.user)
         return qset
 
-class BlogDeleteView(DeleteView,BlogUpdateView):
+
+class BlogDeleteView(DeleteView):
     model = BlogPost
     success_url = reverse_lazy("blog_app:posts")
-    def get_queryset(self, **kwargs):
-        qset = super().get_queryset(**kwargs)
-        qset = qset.filter(owner = self.request.user)
-        return qset
+
+    def get_queryset(self):
+        return super().get_queryset().filter(owner=self.request.user)
 
 
-class BlogPostCreateView:
-    pass
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    fields = ["title", "text",]
+    template_name = 'blog_app/blogpost_create_post.html'
 
-    def form_valid(self,form):
+    def form_valid(self, form):
         print('form_valid called')
         object = form.save(commit=False)
-        object.owner =  self.request.user
+        object.owner = self.request.user
         object.save()
-        return super(BlogPostCreateView,self).form_valid(form)
+        return super(BlogPostCreateView, self).form_valid(form)
 
+    def get_queryset(self, **kwargs):
+        qset = super().get_queryset(**kwargs)
+        qset = qset.filter(owner=self.request.user)
+        return qset
 
-
-
-
+    def get_success_url(self):
+        return reverse_lazy('blog_app:posts')
